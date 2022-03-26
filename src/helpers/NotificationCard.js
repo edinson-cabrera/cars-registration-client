2f52731957588ab4b2439e3a25a8fc465bea6d55
Edinson Cabrera Ñique <urielkillian2607@hotmail.com>
2022-03-26 10:29:57 -0500

    Fixing Unused Imports

--- src/helpers/NotificationCard.js
+++ src/helpers/NotificationCard.js
@@ -1,5 +1,5 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { Fragment, useState } from "react";
+import { Fragment } from "react";
 import { Transition } from "@headlessui/react";
 import { ExclamationCircleIcon } from "@heroicons/react/outline";
 import { XIcon } from "@heroicons/react/solid";
