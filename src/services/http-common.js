8230653753c6674b19a44ae968a527e0ef0eca6f
Edinson Cabrera Ñique <urielkillian2607@hotmail.com>
2022-03-25 20:39:51 -0500

    Header Fix

--- src/services/http-common.js
+++ src/services/http-common.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 export default axios.create({
+  // Development URL (http://localhost:8080/api)
   baseURL: "http://localhost:8080/api",
   headers: {
     "Content-type": "application/json",
